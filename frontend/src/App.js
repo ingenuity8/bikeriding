11ceedae719244024380e366611841f871c48227
Harshavardhan Gavhale <harshgavhale88@gmail.com>
2024-08-30 16:53:18 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 // import Features from './components/Features';
 import Features2 from './components/Features2';
 import HowItWorks from './components/HowItWorks';
-import Pricing from './components/Pricing';
+// import Pricing from './components/Pricing';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 import Hero2 from './components/hero2';
@@ -20,7 +20,7 @@ const HomePage = () => {
             {/* <Features /> */}
             
             <HowItWorks />
-            <Pricing />
+            {/* <Pricing /> */}
             <Testimonials />
             <FAQ/>
             <Footer />
